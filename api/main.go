db77041448bfac448b4b78dff1957e9383d21d79
serious-fin <augustasgri@gmail.com>
2025-06-15 22:14:33 +0300

    make specific problem api endpoint use database

--- api/main.go
+++ api/main.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
@@ -60,39 +59,6 @@ func ErrorHandlerMiddleware() gin.HandlerFunc {
 	}
 }
 
-var problems2 = []problem{
-	{
-		Id:          1,
-		Title:       "Largest Sum",
-		Description: "Find the largest sum foo bar",
-		TestCases: []testCase{
-			{
-				Input:  "[1, 2, 3]",
-				Output: "6",
-			},
-			{
-				Input:  "[]",
-				Output: "0",
-			},
-		},
-	},
-	{
-		Id:          2,
-		Title:       "Maximum sub array",
-		Description: "Find the largest sub-array in the provided array",
-		TestCases: []testCase{
-			{
-				Input:  "[-8, -9, 10, 80, 30, -8]",
-				Output: "[10, 80, 30]",
-			},
-			{
-				Input:  "[]",
-				Output: "0",
-			},
-		},
-	},
-}
-
 var db *sql.DB
 
 func main() {
@@ -124,6 +90,7 @@ func getProblems(c *gin.Context) {
 	rows, err := db.Query("SELECT id, title, description, testCases FROM problems;")
 	if err != nil {
 		c.Error(err)
+		return
 	}
 	defer rows.Close()
 
@@ -134,35 +101,40 @@ func getProblems(c *gin.Context) {
 		err = rows.Scan(&newProblem.Id, &newProblem.Title, &newProblem.Description, &testCasesString)
 		if err != nil {
 			c.Error(err)
+			return
 		}
 		err := json.Unmarshal([]byte(testCasesString), &newProblem.TestCases)
 		if err != nil {
 			c.Error(err)
+			return
 		}
 		problems = append(problems, newProblem)
 	}
 	err = rows.Err()
 	if err != nil {
 		c.Error(err)
+		return
 	}
 	c.IndentedJSON(http.StatusOK, problems)
 }
 
 func getProblemById(c *gin.Context) {
 	id := c.Param("id")
+	row := db.QueryRow("SELECT id, title, description, testCases FROM problems WHERE id = ?;", id)
 
-	// string to int
-	stringifiedId, err := strconv.Atoi(id)
+	var newProblem problem
+	var testCaseString string
+	err := row.Scan(&newProblem.Id, &newProblem.Title, &newProblem.Description, &testCaseString)
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{"message": "Invalid problem id provided. Make sure it is a positive integer"})
+		c.Error(err)
 		return
 	}
 
-	for _, problem := range problems2 {
-		if problem.Id == stringifiedId {
-			c.IndentedJSON(http.StatusOK, problem)
-			return
-		}
+	err = json.Unmarshal([]byte(testCaseString), &newProblem.TestCases)
+	if err != nil {
+		c.Error(err)
+		return
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Problem with id not found"})
+
+	c.IndentedJSON(http.StatusOK, newProblem)
 }
