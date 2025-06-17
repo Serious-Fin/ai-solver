c947396987d932ac485b414e1067ea1d6f524c94
serious-fin <augustasgri@gmail.com>
2025-06-17 22:41:52 +0300

    finish first version of cache

--- api/cache.go
+++ api/cache.go
@@ -1,27 +1,73 @@
 package main
 
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+const (
+	RoleUser      = "user"
+	RoleAssistant = "assistant"
+	RoleSystem    = "system"
+)
+
 type Context struct {
-	role    string
-	content string
+	Role    string
+	Content string
 }
 
-type QueryCache struct {
-	Cache map[string][]Context
+type ContextCache struct {
+	mu      sync.Mutex
+	Cache   map[string][]Context
+	MaxSize int
 }
 
-func (qc *QueryCache) Add(sessionId string, userInput string, machineOutput string) {
-	var context []Context
-	context = qc.Cache[sessionId]
+type TimeRecord struct {
+	LastModified time.Time
+	SessionId    string
+}
 
-	if len(context)+2 <= cap(context) {
-		context = context[2:]
+// maxSize - number of most recent user queries to cache
+func NewContextCache(maxSize int) (*ContextCache, error) {
+	if maxSize < 0 {
+		return nil, errors.New("cache size can not be negative")
 	}
-	context = append(context, Context{
-		role:    "user",
-		content: userInput,
-	})
-	context = append(context, Context{
-		role:    "assistant",
-		content: machineOutput,
-	})
+
+	return &ContextCache{
+		Cache:   make(map[string][]Context),
+		MaxSize: maxSize,
+	}, nil
 }
+
+func (cc *ContextCache) Add(sessionId string, userInput string, aiOutput string) {
+	cc.mu.Lock()
+	defer cc.mu.Unlock()
+
+	cache := cc.Cache[sessionId]
+	cache = append(cache, Context{
+		Role:    RoleUser,
+		Content: userInput,
+	})
+	cache = append(cache, Context{
+		Role:    RoleAssistant,
+		Content: aiOutput,
+	})
+
+	if len(cache) > cc.MaxSize*2 {
+		valuesToRemove := (len(cache) - (cc.MaxSize * 2))
+		cache = cache[valuesToRemove:]
+	}
+	cc.Cache[sessionId] = cache
+}
+
+func (cc *ContextCache) Get(sessionId string) []Context {
+	cc.mu.Lock()
+	defer cc.mu.Unlock()
+	return cc.Cache[sessionId]
+}
+
+/*
+TODO:
+- Implement automatic cache expiration mechanism
+*/
